ef12ab12eb92c842e37229455c9719d0176a9e12
rahmath851 <96294788+rahmath851@users.noreply.github.com>
2022-02-05 17:54:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ secret_key = "bPI0HMXi/RxLQbySm0HRMTevP5unRljaoNYgfEit"
 }
 ####################EC2 INSTANCE##################
 resource "aws_instance" "abc" {
-ami = "ami-08e4e35cccc6189f4"
+ami = "ami-0231217be14a6f3ba"
 instance_type = "t2.micro"
 availability_zone = "us-east-1a"
 key_name = "Ohio"
