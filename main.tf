4a8212e313d927dbd71218bf0d3e8aa3b30579af
rahmath851 <96294788+rahmath851@users.noreply.github.com>
2022-02-05 17:50:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ####################PROVIDER##################
 provider "aws" {
-region = "us-east-1"
+region = "us-east-2"
 access_key = "AKIAZBCAMP7O3OPP5BYB"
 secret_key = "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"
 }
@@ -9,6 +9,6 @@ resource "aws_instance" "abc" {
 ami = "ami-08e4e35cccc6189f4"
 instance_type = "t2.micro"
 availability_zone = "us-east-1a"
-key_name = "nver"
+key_name = "Ohio"
 
 }
